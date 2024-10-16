ea808e96212fbf395a470c8fc8f93392d50e9dba
tws101 <tws101@users.noreply.github.com>
2024-10-16 12:45:12 -0500

    Update worldgenoverride.lua

--- dst_default_config/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
+++ dst_default_config/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
@@ -1,5 +1,7 @@
-return {
-    override_enabled = true,
-    preset = "SURVIVAL_TOGETHER",
-    overrides = {},
+KLEI     1 return {
+	override_enabled = true,
+	worldgen_preset = "SURVIVAL_TOGETHER",
+	settings_preset = "SURVIVAL_TOGETHER",
+	overrides = {
+	},
 }
