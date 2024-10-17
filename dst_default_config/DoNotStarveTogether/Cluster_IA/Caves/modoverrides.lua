350c10060477e525ad393ec8bc808309a0fb85d5
tws101 <tws101@users.noreply.github.com>
2024-10-17 12:13:35 -0500

    Add files via upload

---
+++ dst_default_config/DoNotStarveTogether/Cluster_IA/Caves/modoverrides.lua
@@ -0,0 +1,48 @@
+return {
+--#Your Mod Name Here (Label Things, so you know what they are when you edit them later!) As always, remove the -- to enable!
+--  ["workshop-##########"]={ configuration_options={  }, enabled=true },
+
+--#[API] Gem Core
+  ["workshop-1378549454"]={
+    configuration_options={
+      ["MemSpikeFix:"]=false,
+      MemSpikeFixmaster_override=true,
+      craftinghighlight=false 
+    },
+    enabled=true 
+  },
+
+--#Island Adventures
+  ["workshop-1467214795"]={
+    configuration_options={
+      ["4 Shard Dedicated Servers"]=false,
+      ["Character Refreshes"]=false,
+      ["Cut Content Restoration"]=false,
+      ["Gameplay Features"]=false,
+      ["Items & Structures"]=false,
+      ["Misc."]=false,
+      Mobs=false,
+      devmode=false,
+      droplootground=true,
+      dynamicmusic=true,
+      leif_jungle=true,
+      limestonerepair=true,
+      locale=false,
+      newloot="all",
+      newplayerboats=true,
+      octopustrade=true,
+      oldwarly=false,
+      pondfishable=true,
+      quickseaworthy=false,
+      slotmachineloot=true,
+      tuningmodifiers=true,
+      shipwreckedid="1",
+      volcanoid="2",
+      forestid="3",
+      caveid="4",
+      windgustable="all",
+      windstaffbuff=2 
+    },
+    enabled=true 
+  }
+}
